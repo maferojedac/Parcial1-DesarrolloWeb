44e8765cbe25536297465e1f8a0c1a5dfa5df695
Maria Fernanda Ojeda Carrillo <0228660@up.edu.mx>
2022-09-19 19:57:59 -0500

    lo que logré

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './App.css';
 
+/* 
+const userComponent=() => {
+  const formik = useFormik ({
+    user: {
+      nombre:'',
+      apellido:'',
+      apodo:''
+    },
+    onSubmit:values=>{
+      const etiqueta = (JSON.stringify(values))
+    }
+  })
+}
+ */
 function App() {
+
+  const [nombreInput, setNombreInput] = useState("");
+  const [apellidoInput, setApellidoInput] = useState("");
+  const [apodoInput, setApodoInput] = useState("");
+
+  const user = {
+    nombre:{nombreInput},
+    apellido:{apellidoInput},
+    apodo:{apodoInput},
+  }
+
+  const inputChangeNombre = (event) => {
+    setNombreInput(event.target.value);
+  }
+
+  const inputChangeApellido = (event) => {
+    setApellidoInput(event.target.value);
+  }
+
+  const inputChangeApodo = (event) => {
+    setApodoInput(event.target.value);
+  }
+
+  const validar = () => {
+    if(nombreInput==null || apodoInput==null || apellidoInput==null) {}
+    else()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+      <form action="/form" method="post">
+
+        <label for="nombre">Nombre:</label>
+        <input type="text" value={nombreInput} onChange={inputChangeNombre}/>
+
+        <label for="apellido">Apellido:</label>
+        <input type="text" value={apellidoInput} onChange={inputChangeApellido}/>
+
+        <label for="apodo">Apodo:</label>
+        <input type="text" value={apodoInput} onChange={inputChangeApodo}/>
+
+        <button type="reset" >Limpiar</button>
+
+        <button disabled={disabled}> Enviar </button>
+
+
+      </form>
+
+      
+      const etiqueta = JSON.stringify(user);
+
+        
+     </>
   );
 }
 
